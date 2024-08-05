93b466b4d0d02493eb76428fa692bb7188021fba
saad <sayedsaadhusain@gmail.com>
2024-08-05 14:59:32 +0530

    contact page added again with proper form

--- js/main.js
+++ js/main.js
@@ -100,5 +100,16 @@
     });
 
 
+    // Force Scroll to the Top of the Page on Page Reload
+    if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+    } else {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    }
+    
+
+
 })(jQuery);
 
