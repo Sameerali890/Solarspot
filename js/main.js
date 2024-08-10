39ec71a4f375f6bfb4894965fc013dd51ed80563
Sameerali890 <141541073+Sameerali890@users.noreply.github.com>
2024-08-10 11:07:01 +0530

    svg animation

--- js/main.js
+++ js/main.js
@@ -125,3 +125,24 @@ gsap.from(".header-carousel .anim-text h4",{
     duration:2,
 })
 
+var path = "M 10 100 Q 500 100 990 100";
+var finalPath = "M 10 100 Q 500 100 990 100";
+
+var string = document.querySelector(".line");
+string.addEventListener("mousemove", function (dets) {
+  path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`;
+
+  gsap.to("svg path", {
+    attr: { d: path },
+    duration: 0.3,
+    ease: "power3.out",
+  });
+});
+string.addEventListener("mouseleave", function (){
+    gsap.to("svg path", {
+    attr: { d: finalPath },
+    duration: 1.5,
+    ease: "elastic.out(1.3)",
+  });
+})
+
